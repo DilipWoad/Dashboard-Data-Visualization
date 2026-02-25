c9207829ca85300d34e44cd4d1590d66155d2607
Dilip Woad <dilipwoad777@gmail.com>
2026-02-25 16:03:49 +0530

    frontend : complete frontend setup in vite+react ,tailwind and axios.

---
+++ dashboard-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-gray-700 text-white text-4xl font-extrabold w-screen h-screen text-center">
+        Hello World
+      </h1>
+    </>
+  );
+}
+
+export default App;
