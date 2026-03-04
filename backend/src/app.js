3c214ba8b89e5eaf545946d5e72e7bfd7eb9c39c
Dilip Woad <dilipwoad777@gmail.com>
2026-03-04 16:46:53 +0530

    add health check in the backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,6 +21,13 @@ app.use(express.urlencoded({limit:"20kb",extended:true}));
 //Handling routing 
 import dataRouter from "./routes/data.route.js"
 
+app.get('/',(req,res)=>{
+    res.status(200).send({
+        statusCode : 200,
+        message:"Everthing OK"
+    })
+})
+
 app.use('/api/v1/data',dataRouter)
 //
 
